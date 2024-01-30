3fbbd2fadb269bce13adc14cd6dbac5a0c9cd0a9
Sahil <seekersahil@gmail.com>
2024-01-30 22:03:34 +0530

    adjusted product cards' widths

--- pagination-app/src/app/components/ProductCard/index.tsx
+++ pagination-app/src/app/components/ProductCard/index.tsx
@@ -4,7 +4,7 @@ import { ProductCardProps } from "./ProductCard.types";
 const ProductCard = (props: ProductCardProps) => {
   const { name, photo_url, price, description, category } = props;
   return (
-    <div className="border-2 border-black rounded-lg p-6 flex flex-col gap-3 place-content-center place-items-center">
+    <div className="border-2 border-black rounded-lg p-6 flex flex-col gap-3 place-content-center place-items-center w-80 min-h-96">
       <Image src={photo_url} alt={name} width={300} height={300} />
       <div className="metadata flex justify-between w-full">
         <h1 className="font-bold">{name}</h1>
